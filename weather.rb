9147fac88a631063af0472701cea3185a5a0d76c
Britni <britnirjones@gmail.com>
2017-03-21 15:28:59 -0400

    Gets the current conditions. Adds tests. Tests initialize method and current conditions against Durham information successfully.

--- weather.rb
+++ weather.rb
@@ -1,14 +1,21 @@
 require 'httparty'
 
-class weather
+class Weather
   attr_reader :zipcode
-  def initialize(@zipcode)
-  
+  def initialize(number)
+    @zipcode = number
+    #if number != ##### then puts this is not a valid zipcode 
+  end
+
+  def get_weather
+    weather = HTTParty.get("http://api.wunderground.com/api/8cd8b07cf03f2c20/conditions/q/#{@zipcode}.json")
+    weather["current_observation"]["display_location"]
+  end
+
 end
 
-puts "What is your Zip Code?"
-zipcode = gets.chomp
 
-weather = HTTParty.get("http://api.wunderground.com/api/8cd8b07cf03f2c20/conditions/q/#{zipcode}.json")
+#puts "Enter your zipcode!"
+#zip = Weather.new(gets.chomp).get_weather
 
-puts weather["current_observation"]["display_location"]
+#puts zip
---
+++ weather_test.rb
@@ -0,0 +1,13 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './weather.rb'
+
+class WeatherReportTest < Minitest::Test
+  def test_initialize
+    assert Weather.new(27701)
+  end
+
+  def test_get_weather
+    assert Weather.new(27701).get_weather["full"] == "Durham, NC"
+  end
+end
