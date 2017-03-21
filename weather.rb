c1bb9a54df04a7ba6b1eb2e633611766bcb1964e
Britni <britnirjones@gmail.com>
2017-03-21 15:58:38 -0400

    updated my weather.rb file to include appropriate variables. Same name variables were conflicting with Class name.

--- weather.rb
+++ weather.rb
@@ -4,18 +4,23 @@ class Weather
   attr_reader :zipcode
   def initialize(number)
     @zipcode = number
-    #if number != ##### then puts this is not a valid zipcode 
+
+    #if number != ##### then puts this is not a valid zipcode
   end
 
   def get_weather
-    weather = HTTParty.get("http://api.wunderground.com/api/8cd8b07cf03f2c20/conditions/q/#{@zipcode}.json")
-    weather["current_observation"]["display_location"]
+    weather_lookup = HTTParty.get("http://api.wunderground.com/api/8cd8b07cf03f2c20/conditions/q/#{@zipcode}.json")
+    weather_lookup["current_observation"]["display_location"]
   end
 
+  def forecast
+    #@weather
+    #weather["current_observation"]
+  end
 end
 
 
-#puts "Enter your zipcode!"
-#zip = Weather.new(gets.chomp).get_weather
+puts "Enter your zipcode!"
+zip = Weather.new(gets.chomp).get_weather
 
-#puts zip
+puts zip
