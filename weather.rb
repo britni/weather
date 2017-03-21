a09a1912f9802f2c04e91627bd43f65d7e4ccc51
Britni <britnirjones@gmail.com>
2017-03-21 19:43:34 -0400

    My tests passed! It was the puts in the method! -_-

--- weather.rb
+++ weather.rb
@@ -4,23 +4,15 @@ class Weather
   attr_reader :zipcode
   def initialize(number)
     @zipcode = number
-
-    #if number != ##### then puts this is not a valid zipcode
+    weather_lookup_raw = HTTParty.get("http://api.wunderground.com/api/8cd8b07cf03f2c20/conditions/q/#{@zipcode}.json")
+    @weather_lookup = weather_lookup_raw["current_observation"]
   end
 
   def get_weather
-    weather_lookup = HTTParty.get("http://api.wunderground.com/api/8cd8b07cf03f2c20/conditions/q/#{@zipcode}.json")
-    weather_lookup["current_observation"]
+    @weather_lookup
   end
 
   def forecast
-    get_weather
-    puts "The weather in #{get_weather["display_location"]["full"]} is #{get_weather["temp_f"]} degrees Fahreinheit and #{get_weather["weather"]}. Winds are #{get_weather["wind_string"]} so it actually feels like #{get_weather["feelslike_f"]} degrees."
+    "The weather in #{@weather_lookup["display_location"]["full"]} is #{@weather_lookup["temp_f"]} degrees Fahreinheit and #{@weather_lookup["weather"]}. Winds are #{@weather_lookup["wind_string"]} so it actually feels like #{@weather_lookup["feelslike_f"]} degrees."
   end
 end
-
-
-#puts "Enter your zipcode!"
-zip = Weather.new(27713).forecast
-
-puts zip
--- weather_test.rb
+++ weather_test.rb
@@ -2,12 +2,16 @@ require 'minitest/autorun'
 require 'minitest/pride'
 require './weather.rb'
 
-class WeatherReportTest < Minitest::Test
+class WeatherTest < Minitest::Test
   def test_initialize
     assert Weather.new(27701)
   end
 
   def test_get_weather
-    assert Weather.new(27701).get_weather["display_location"]["full"] == "Durham, NC"
+    assert_equal Weather.new(27701).get_weather["display_location"]["full"], "Durham, NC"
+  end
+
+  def test_forecast
+    assert_equal Weather.new(27701).forecast, "The weather in Durham, NC is 72.6 degrees Fahreinheit and Clear. Winds are Calm so it actually feels like 72.6 degrees."
   end
 end
