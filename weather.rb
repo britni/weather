0abe0478154c442d6a2db9975d33a7338d6150f7
Britni <britnirjones@gmail.com>
2017-03-21 14:58:06 -0400

    testing push

--- weather.rb
+++ weather.rb
@@ -1,1 +1,14 @@
-puts "Weather Report from my desk"
+require 'httparty'
+
+class weather
+  attr_reader :zipcode
+  def initialize(@zipcode)
+  
+end
+
+puts "What is your Zip Code?"
+zipcode = gets.chomp
+
+weather = HTTParty.get("http://api.wunderground.com/api/8cd8b07cf03f2c20/conditions/q/#{zipcode}.json")
+
+puts weather["current_observation"]["display_location"]
