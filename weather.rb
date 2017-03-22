70ce84ff3a0abbffad9b5e38da449190ec4867dc
Britni <britnirjones@gmail.com>
2017-03-21 23:48:27 -0400

    Hid the key.

--- weather.rb
+++ weather.rb
@@ -4,7 +4,7 @@ class Weather
   attr_reader :zipcode
   def initialize(number)
     @zipcode = number
-    weather_lookup_raw = HTTParty.get("http://api.wunderground.com/api/8cd8b07cf03f2c20/conditions/q/#{@zipcode}.json")
+    weather_lookup_raw = HTTParty.get("http://api.wunderground.com/api/#{apikey}/conditions/q/#{@zipcode}.json")
     @weather_lookup = weather_lookup_raw["current_observation"]
   end
 
@@ -15,4 +15,10 @@ class Weather
   def forecast
     "The weather in #{@weather_lookup["display_location"]["full"]} is #{@weather_lookup["temp_f"]} degrees Fahreinheit and #{@weather_lookup["weather"]}. Winds are #{@weather_lookup["wind_string"]} so it actually feels like #{@weather_lookup["feelslike_f"]} degrees."
   end
+
+  def apikey
+    JSON.parse(File.read('key.json'))['apikey']
+  end
 end
+
+puts Weather.new(27701).forecast
